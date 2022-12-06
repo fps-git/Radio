5805c295fbd8cb0585fc749618d5a10feae47d56
Pavel Fomin <mail4fps@gmail.com>
2022-12-06 02:30:21 +0100

    Release 1.1

--- src/main/java/ru/netology/Radio/Radio.java
+++ src/main/java/ru/netology/Radio/Radio.java
@@ -1,8 +1,31 @@
 package ru.netology.Radio;
 
 public class Radio {
+    private int numberOfStations = 10;
+    private int minVolumeLevel = 0;
+    private int maxVolumeLevel = 100;
     private int stationNumber;
     private int volumeLevel = 1; //Зададим значение громкости по умолчанию при включении радио
+
+    public Radio(){
+
+    }
+    public Radio(int numberOfStations) {
+        //Ограничим максимальное количество станций осмысленным числом с запасом
+        if (numberOfStations > 1 && numberOfStations <= 1000) {
+            this.numberOfStations = numberOfStations;
+        } else {
+            if (numberOfStations < 1) { //Если будет задано нулевое или отрицательное кол-во то установим по умолчанию
+                this.numberOfStations = 10;
+            } else {
+                this.numberOfStations = 1000; //Если будет задано больше 1000 то установим 1000
+            }
+
+        }
+    }
+    public int getNumberOfStations() {
+        return numberOfStations;
+    }
 
     public int getStationNumber() {
         return stationNumber;
@@ -12,14 +35,14 @@ public class Radio {
         if (stationNumber < 0) {
             return;
         }
-        if (stationNumber > 9) {
+        if (stationNumber >= numberOfStations) {
             return;
         }
         this.stationNumber = stationNumber;
     }
 
     public void next() {
-        if (stationNumber == 9) {
+        if (stationNumber == (numberOfStations - 1)) {
             setStationNumber(0);
         } else {
             setStationNumber(stationNumber + 1);
@@ -28,7 +51,7 @@ public class Radio {
 
     public void prev() {
         if (stationNumber == 0) {
-            setStationNumber(9);
+            setStationNumber(numberOfStations - 1);
         } else {
             setStationNumber(stationNumber - 1);
         }
@@ -39,23 +62,23 @@ public class Radio {
     }
 
     public void setVolumeLevel(int volumeLevel) {
-        if (volumeLevel < 0) {
+        if (volumeLevel < minVolumeLevel) {
             return;
         }
-        if (volumeLevel > 10) {
+        if (volumeLevel > maxVolumeLevel) {
             return;
         }
         this.volumeLevel = volumeLevel;
     }
 
     public void volumeUp() {
-        if (volumeLevel < 10) {
+        if (volumeLevel < maxVolumeLevel) {
             volumeLevel = volumeLevel + 1;
         }
     }
 
     public void volumeDown() {
-        if (volumeLevel > 0) {
+        if (volumeLevel > minVolumeLevel) {
             volumeLevel = volumeLevel - 1;
         }
     }
